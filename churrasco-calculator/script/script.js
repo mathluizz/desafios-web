8eb1d590e15b5492f797612beea00e293d6db1a0
Matheus Oliveira <matheus_luiz2011@hotmail.com>
2023-02-01 13:20:25 -0300

    churrasco initial commit 2.0

---
+++ churrasco-calculator/script/script.js
@@ -0,0 +1,10 @@
+//carne - 400gr/pessoa | +6h 650gr
+//cerveja - 1200ml/pessoa | +6h 2000ml
+//refirgerante - 1000ml/pessoa | +6h 1500ml
+
+//crianças valem 0.5
+
+function calcular(){
+    // let adult = Document.
+    console.log("Olá mundo")
+}
