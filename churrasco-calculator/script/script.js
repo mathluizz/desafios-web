c25b2b52762a0686e2b141dabef4506fb3cb09b7
Matheus Oliveira <matheus_luiz2011@hotmail.com>
2023-02-02 00:39:58 -0300

    churrasco-calculator v1.0

--- churrasco-calculator/script/script.js
+++ churrasco-calculator/script/script.js
@@ -5,6 +5,45 @@
 //crianças valem 0.5
 
 function calcular(){
-    // let adult = Document.
-    console.log("Olá mundo")
+    let adult = document.getElementById("adultos").value
+    let child = document.getElementById("criancas").value
+    let duration = document.getElementById("duracao").value
+
+    let result = document.getElementById("result")
+    
+
+    let carne, cerveja, refirgerante
+    carne = carnePP(duration) * adult + ((carnePP(duration) * 0.5) * child)
+    refirgerante = refriPP(duration) * adult + ((refriPP(duration) * 0.5) * child)
+    cerveja = cervejaPP(duration) * adult 
+
+
+    result.innerHTML = `<p> ${carne/1000} kg de carne </p>`
+    result.innerHTML += `<p>${Math.ceil(cerveja/350)} latas de cerveja</p>`
+    result.innerHTML += `<p>${Math.ceil(refirgerante/2000)} Pet's 2L de refri</p>`
+    
+}
+
+function carnePP (duration){
+    if (duration >= 6){
+        return 650
+    } else {
+        return 400
+    }
+}
+
+function cervejaPP (duration){
+    if (duration >= 6){
+        return 2000
+    } else {
+        return 1200
+    }
+}
+
+function refriPP (duration){
+    if (duration >= 6){
+        return 1500
+    } else {
+        return 1000
+    }
 }
